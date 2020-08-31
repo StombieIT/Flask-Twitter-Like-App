c0dc888823cadb9f4990cf4b815f6f651c715fbc
StombieIT <65386839+StombieIT@users.noreply.github.com>
2020-08-31 12:48:15 +0300

    Update app.py

--- appy/app.py
+++ appy/app.py
@@ -4,6 +4,8 @@ from services.admin import admin
 from services.login_manager import login_manager
 from services.mail import mail
 from services.db import db
+from services.migrate import migrate
+from blueprints.api.api import api
 from blueprints.auth.auth import auth
 from blueprints.post.post import post
 
@@ -15,7 +17,9 @@ admin.init_app(app)
 login_manager.init_app(app)
 mail.init_app(app)
 db.init_app(app)
+migrate.init_app(app, db)
 
 # Blueprints
 app.register_blueprint(auth, url_prefix='/auth')
 app.register_blueprint(post, url_prefix='/post')
+app.register_blueprint(api, url_prefix='/api')

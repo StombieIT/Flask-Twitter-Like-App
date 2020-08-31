942022e3f45cb88bdb723691af9b9da9f7490a1e
StombieIT <65386839+StombieIT@users.noreply.github.com>
2020-08-31 12:45:02 +0300

    Update manage.py

--- appy/manage.py
+++ appy/manage.py
@@ -1,14 +1,11 @@
 from flask_script import Manager
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import MigrateCommand
 from app import app
 from views import (
 	index,
 	page_not_found,
 	forbidden
 )
-from services.db import db
-
-migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
